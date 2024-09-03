daa6408031a18ef00945c35fea90ee9d792ec3bd
Caio Petrucci Rosa <caiopetruccirosa@gmail.com>
2024-09-03 00:13:56 -0300

    add permission rsync option to sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -144,8 +144,9 @@ main() {
     # -v: verbose
     # -z: compress file data during the transfer
     # -h: human-readable output
+    # -p: preserve permissions
     # --fuzzy: find similar file names during the syncing process
-    sync_opts="-rltDvzh --fuzzy"
+    sync_opts="-rltDvzhp --fuzzy"
 
     # prints the syncing folders
     echo "Syncing $local_path to $remote_server_path"
